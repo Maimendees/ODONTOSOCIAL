450447739c11abfe1849a23113d0eafd2fcf141e
Maimendees <maiara_mendes.13@hotmail.com>
2025-11-30 21:01:04 -0300

    atualização

---
+++ 01_createTable.sql
@@ -0,0 +1,58 @@
+BEGIN TRANSACTION;
+
+
+create table ong (
+id_ong INTEGER PRIMARY KEY AUTOINCREMENT,
+nome TEXT NOT NULL,
+telefone TEXT(15)
+); 
+
+-----------tabela paciente------
+create table paciente (
+id_paciente INTEGER PRIMARY KEY AUTOINCREMENT,
+nome TEXT (100),
+idade INTEGER,
+telefone TEXT(15),
+id_ong INTEGER,
+FOREIGN KEY (id_ong) REFERENCES ong(id_ong)
+);
+
+-------------tabela dentista-----------
+
+create table dentista (
+id_dentista INTEGER PRIMARY KEY AUTOINCREMENT,
+nome TEXT NOT NULL,
+crm TEXT NOT NULL,
+especialidade TEXT(100)
+);
+
+-----------------tabela consulta-----------
+create table consulta (
+id_consulta INTEGER PRIMARY KEY AUTOINCREMENT,
+data TEXT NOT NULL,
+procedimento TEXT(100),
+id_paciente INTEGER,
+id_dentista INTEGER,
+FOREIGN KEY (id_paciente) REFERENCES paciente(id_paciente)
+FOREIGN KEY (id_dentista) REFERENCES dentista(id_dentista)
+);
+
+-------------------tabela medicamento----------
+create table medicamento (
+id_medicamento INTEGER PRIMARY KEY AUTOINCREMENT,
+nome TEXT NOT NULL,
+data TEXT,
+quantidade INTEGER,
+id_consulta INTEGER,
+FOREIGN KEY (id_consulta) REFERENCES consulta(id_consulta)
+);
+
+------------tabela medicamento_consulta----------
+create table medicamento_consulta (
+id_medicamento INTEGER,
+id_consulta INTEGER,
+id_dentista,
+FOREIGN KEY (id_medicamento) REFERENCES medicamento(id_medicamento),
+FOREIGN KEY (id_consulta) REFERENCES consulta(id_consulta),
+FOREIGN KEY (id_dentista) REFERENCES dentista(id_dentista)
+);
